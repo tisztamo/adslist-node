838cac43d2ae6e02095ba08eb22c8db778325411
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2015-06-02 12:17:45 +0200

    removed dangerous and unnecessary filter 'totrust'

--- client/app/ads/totrusted.js
+++
@@ -1,7 +0,0 @@
-angular.module('adslistApp')
-  .filter('totrusted', ['$sce', function ($sce) {
-    'use strict';
-    return function (text) {
-      return $sce.trustAsHtml(text);
-    };
-  }]);
